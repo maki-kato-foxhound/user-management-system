4727bfe89b6b5524714d39ffc443ba2f7a3e1d8e
maki-kato-foxhound <maki.kato@fox-hound.jp>
2025-08-12 16:20:11 +0900

    2-2完了！

---
+++ components/EditUserForm.stories.tsx
@@ -0,0 +1,23 @@
+// components/EditUserForm.stories.tsx
+
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+import EditUserForm from "./EditUserForm";
+
+// TODO: メタデータ
+const meta: Meta<typeof EditUserForm> = {
+  title: "Components/EditUserForm",
+  component: EditUserForm,
+};
+export default meta;
+
+// TODO: ストーリーの定義
+type Story = StoryObj<typeof EditUserForm>;
+
+export const Default: Story = {
+  args: {
+    userId: 1, // 例となるユーザーID
+    onSuccess: () => alert('更新成功しました！'),
+    onError: (error) => alert(`更新失敗: ${error.message}`),
+  },
+};
