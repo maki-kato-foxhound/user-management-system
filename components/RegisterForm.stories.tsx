05d07c01d742d0e85444c538b3111d9f6e204f29
maki-kato-foxhound <maki.kato@fox-hound.jp>
2025-08-12 12:21:24 +0900

    タスク1-2完了！

---
+++ components/RegisterForm.stories.tsx
@@ -0,0 +1,24 @@
+// components/RegisterForm.stories.tsx
+
+import React from "react";
+import type { Meta, StoryObj } from "@storybook/react";
+import RegisterForm from "./RegisterForm";
+
+// TODO: メタデータ
+const meta: Meta<typeof RegisterForm> = {
+  title: "Components/RegisterForm",
+  component: RegisterForm,
+};
+export default meta;
+
+// TODO: ストーリーの定義
+type Story = StoryObj<typeof RegisterForm>;
+
+// TODO: デフォルトストーリーの設定
+export const Default: Story = {
+  args: {
+    disabled: false,
+    onSuccess: () => alert('登録成功しました！'),
+    onError: (error) => alert(`登録失敗: ${error.message}`),
+  },
+};
